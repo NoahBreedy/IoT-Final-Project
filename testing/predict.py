c6ecf6c40c5150d0d34c70267fbf5a7df29eff3c
Mj <None>
2026-05-03 18:56:03 -0400

    Added webcam and bot streams

--- testing/predict.py
+++ testing/predict.py
@@ -144,6 +144,11 @@ class StreamData:
 
 
 stream_data:dict[StreamData] = {
+    "CamFull": StreamData("http://localhost:8080/stream/0"),
+    "CamBL": StreamData("http://localhost:8080/stream/1"),
+    "CamTL": StreamData("http://localhost:8080/stream/2"),
+    "CamBR": StreamData("http://localhost:8080/stream/3"),
+    "CamTR": StreamData("http://localhost:8080/stream/4"),
     "0": StreamData("http://172.20.10.9:81/stream"),
     "1": StreamData("http://172.20.10.8:81/stream"),
     "2": StreamData("http://172.20.10.7:81/stream"),
@@ -223,6 +228,8 @@ class StreamHandler(BaseHTTPRequestHandler):
         except (BrokenPipeError, ConnectionResetError):
             print(f"Client disconnected from path {self.path}")
             pass
+
+        print(f"Connection ended with client from path {self.path}")
 
         
     
